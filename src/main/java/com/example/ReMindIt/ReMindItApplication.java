6ef701033ce1cd0ab47bf7f37860fab8c3d2b327
Chiranjiv007 <chiranjiv@iitg.ernet.in>
2020-02-19 12:06:14 +0530

    Initial Commit

---
+++ src/main/java/com/example/ReMindIt/ReMindItApplication.java
@@ -0,0 +1,13 @@
+package com.example.ReMindIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReMindItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReMindItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ReMindIt/ReMindItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ReMindIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReMindItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
